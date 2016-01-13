9a9f0ad7f48f049427b25aff37e6cb6976f533ea
duncangrubbs <duncan.grubbs@gmail.com>
2016-01-13 10:59:24 -0800

    Add object and array test.

--- src/com/duncangrubbs/bridge/Card.java
+++ src/com/duncangrubbs/bridge/Card.java
@@ -4,4 +4,7 @@ package com.duncangrubbs.bridge;
  * Created by Duncan on 1/12/2016.
  */
 public class Card {
+
+    public Card card1 = new Card();
+
 }
--- src/com/duncangrubbs/bridge/Hand.java
+++ src/com/duncangrubbs/bridge/Hand.java
@@ -4,4 +4,8 @@ package com.duncangrubbs.bridge;
  * Created by Duncan on 1/12/2016.
  */
 public class Hand {
+
+    Card[] hand = new Card[12];
+
+    hand[0] = new Card card1;
 }
